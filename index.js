ce3cd0ca9580844a6ab03a41a84d24ba7766d4cb
Gagge <tobias@nublar.se>
2017-03-01 22:04:51 +0100

    Half done

--- index.js
+++ index.js
@@ -1,67 +1,133 @@
 'use strict';
 
-// This file is ment to be run from PM2 to start a http server
-// To test the server, use a custom script with configs to load
-// the server model in a convenient way
-
-// Make sure this is ran from the correct folder
-process.chdir(__dirname);
-
-const	subscriptions	= require('./config/subscriptions.json'),
-	amqpConf	= require('./config/amqp.json'),
-	logConf	= require('./config/log.json'),
-	Intercom	= require('larvitamintercom'),
-	intercom	=	new Intercom(amqpConf),
-//	mailer	= require('larvitmail'),
+const	EventEmitter	= require('events').EventEmitter,
+	eventEmitter	= new EventEmitter(),
+	lUtils	= require('larvitutils'),
 	async	= require('async'),
 	log	= require('winston'),
 	fs	= require('fs'),
 	_	= require('lodash');
 
 
-// Add support for daily rotate file
-log.transports.DailyRotateFile = require('winston-daily-rotate-file');
+let	readyInProgress	= false,
+	isReady	= false,
+	intercom;
 
-// Handle logging from config file
-log.remove(log.transports.Console);
-if (logConf !== undefined) {
-	for (const logName of Object.keys(logConf)) {
-		if (typeof logConf[logName] !== Array) {
-			logConf[logName] = [logConf[logName]];
-		}
+function ready(cb) {
+	const	tasks	= [];
 
-		for (let i = 0; logConf[logName][i] !== undefined; i ++) {
-			log.add(log.transports[logName], logConf[logName][i]);
-		}
+	if (isReady === true) { cb(); return; }
+
+	if (readyInProgress === true) {
+		eventEmitter.on('ready', cb);
+		return;
 	}
+
+	readyInProgress = true;
+
+	// Set intercom
+	tasks.push(function(cb) {
+		intercom	= lUtils.instances.mailerIntercom;
+		cb();
+	});
+
+	async.series(tasks, function() {
+		isReady	= true;
+		eventEmitter.emit('ready');
+		cb();
+	});
 }
 
-intercom.ready(function() {
-	const	tasks	= [];
-	for (let exchange in subscriptions) {
-		tasks.push(function(cb) {
-			const	options	= {'exchange': exchange};
-			intercom.subscribe(options, function(message, ack) {
-				ack();
-				if (subscriptions[exchange].actions.includes(message.action)) {
-					getTemplate(message.action, function(err, template) {
-						let	mail	= _.template(template);
-						console.log(mail({'data': message.action}));
-					});
-				}
-			}, function(err) {
-				cb(err);
-			});
-		});
-	}
+function Mailer(options) {
+	this.ready	= ready; // To expose to the outside world
+	this.subscriptions 	= options.subscriptions;
+	this.mailConfig	=	options.mailConfig;
+};
 
-	async.parallel(tasks, function(err) {
-		if (err) throw err;
+
+Mailer.prototype.start = function(cb) {
+	const	tasks	= [],
+		that	= this;
+
+	tasks.push(ready);
+
+
+	tasks.push(function(cb) {
+		const	tasks	= [];
+		for (let exchange in that.subscriptions) {
+			tasks.push(function(cb) {
+				const	options	= {'exchange': exchange};
+				intercom.subscribe(options, function(message, ack) {
+					ack();
+
+					if (that.subscriptions[exchange].actions.includes(message.action)) {
+						const	tasks	= [];
+
+						let	templateData;
+
+						// Looking for data extension
+						tasks.push(function (cb) {
+							const extensionPath = process.cwd() + '/extensions/' + exchange + '/' + message.action + '.js';
+
+							fs.stat(extensionPath, (err) => {
+								if (err) {
+									log.info('larvitammail - index.js: No data extension found for action ' + message.action);
+									cb(null);
+								} else {
+									let extension;
+									log.info('larvitammail - index.js: Data extension found for action ' + message.action);
+									extension = require(extensionPath);
+									extension.run(message.params, function(err, data) {
+										templateData = data;
+										cb(err);
+									});
+								}
+							});
+						});
+
+						// Get render template
+						tasks.push(function (cb) {
+							getTemplate(message.action, function (err, template) {
+								if (err) { cb(err); return; };
+								let	mail	= _.template(template);
+								console.log(mail({'data': templateData}));
+								cb();
+							});
+						});
+
+						// Send email.
+						tasks.push(function (cb) {
+							console.log('Sending email...');
+							cb();
+						});
+
+						async.series(tasks, function(err) {
+							if (err) { cb(err); return; };
+						});
+
+					}
+
+				}, function(err) {
+					cb(err);
+				});
+			});
+		}
+
+		async.series(tasks, function(err) {
+			cb(err);
+		});
 	});
-});
+
+	async.series(tasks, function(err) {
+		cb(err);
+	});
+};
 
 function getTemplate(action, cb) {
-	fs.readFile('./templates/' + action + '.tmpl', (err, data) => {
+	fs.readFile(process.cwd() + '/templates/' + action + '.tmpl', (err, data) => {
 		cb(err, data);
 	});
 };
+
+
+exports = module.exports = Mailer;
